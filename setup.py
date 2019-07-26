35f5173c1d114dd09ac4cd64f6a8d560b75c0c31
david-cortes <david.cortes.rivera@gmail.com>
2019-07-26 19:28:03 +0300

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
    'costsensitive',
    'joblib>=0.13'
 ],
-  version = '0.1.7.7',
+  version = '0.1.7.8',
   description = 'Python Implementations of Algorithms for Contextual Bandits',
   author = 'David Cortes',
   author_email = 'david.cortes.rivera@gmail.com',
