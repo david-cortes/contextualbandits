b676a9bf22b764794cb2ebcb651e95f2fb3a2a39
David Cortes <david.cortes.rivera@gmail.com>
2022-11-11 16:11:06 +0100

    fix potentially wrong calculation when using both beta_prior=0..+smoothing

--- contextualbandits/utils.py
+++ contextualbandits/utils.py
@@ -944,7 +944,11 @@ class _OneVsRest:
 
         ## Note: if it has smoothing, it will end up applying smoothing afterwards,
         ## so the prediction will not be exactly zero.
-        if not self.force_unfit_predict and self.smooth is not None and self.counters[0, choice] == 0:
+        if (
+            not self.force_unfit_predict
+            and self.smooth is not None and self.counters[0, choice] == 0
+            and not isinstance(self.algos[choice], _FixedPredictor)
+        ):
             preds[:, choice] = 0
             return None
 
